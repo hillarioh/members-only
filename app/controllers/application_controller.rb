f53dd87517599ddfb25aab2ce925b8ac21378bae
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-06-10 15:47:44 +0600

    Create post params and build

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
-    private
-
-    def post_params
-        params.require(:post).permit(:title, :text, :user_id)
-    end
+   
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,16 +2,21 @@ class PostsController < ApplicationController
     before_action :authenticate_user!, only: %i[new create]
 
     def new
-        @post = Post.new
+        @post = current_user.posts.build
     end
 
     def create
-        @post = Post.new(post_params)
-        @post.user_id = current_user
+        @post = current_user.posts.build(post_params)
         if @post.save
             redirect_to root_path
         else
             render :new
         end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :content, :user_id)
     end
 end
