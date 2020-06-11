017ec155fcf520a749d9684efa2358906bd49d6f
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-06-11 14:08:44 +0600

    Fix rubocop errors

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -9,10 +9,10 @@ module PostsHelper
 
   def index_session
     if user_signed_in?
-      link_to('Logout', destroy_user_session_path, method: :delete,:class=>"btn btn-secondary")
+      link_to('Logout', destroy_user_session_path, method: :delete, class: 'btn btn-secondary')
     else
-      link_to('Login', new_user_session_path,:class=>"btn btn-secondary")
-      link_to('Register', new_user_registration_path,:class=>"btn btn-secondary")
+      link_to('Login', new_user_session_path, class: 'btn btn-secondary')
+      link_to('Register', new_user_registration_path, class: 'btn btn-secondary')
     end
   end
 end
